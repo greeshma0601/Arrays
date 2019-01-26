4161c74cfff5f7bb7bdb9dd5486fe014e0219523
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-26 23:09:15 +0530

    Create Doubling the value.cpp

---
+++ Doubling the value.cpp
@@ -0,0 +1,61 @@
+/*
+Doubling the value
+Given an array of size n and an integer b, traverse the array and if the element in array is b, double b and continue traversal. In the end return value of b.
+
+Input:
+
+The first line of the input contains T denoting the number of testcases. Then follows the description of testcases. The first line of each testcase contains two space separated positive integers n and b denoting the size of array and initial value of b respectively.The second line contains n space separated positive integers denoting the elements of array.
+
+
+Output:
+
+For each testcase, print the final value of b.
+
+
+Constraints:
+
+1<=T<=100
+1<=n<=50
+1<=b<=1000
+1<=A[i]<=1018 (1<=i<=N)
+
+
+Example:
+
+Input:
+1
+5 2
+1 2 3 4 8
+
+Output:
+16
+
+Explanation: The initial value of b is 2. Traversing from the first element, 2 is found in 2nd position, so doubling b we get b = 4 . 4 is found in 4th position so doubling b we get b = 8. 8 is found in 5th position, so doubling b we get b = 16.
+*/
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+ {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+long long int i,n,b;
+ cin>>n>>b;
+long long int a[n];
+ for(i=0;i<n;i++)
+ cin>>a[i];
+ for(i=0;i<n;i++)
+ {
+     if(a[i] == b)
+     {
+         b=b*2;
+     }
+ }
+ cout<<b<<endl;
+ }
+	//code
+	return 0;
+}
