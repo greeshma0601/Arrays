fcdcb194210b5170c3a7bd11f64dfa74552a66f7
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-03-30 23:43:35 +0530

    Create Anshuman's Favourite Number.cpp

---
+++ Mathematical/Anshuman's Favourite Number.cpp
@@ -0,0 +1,64 @@
+/*
+Anshuman's Favourite Number
+You are given an integer input N and you have to find whether it is Anshuman’s favourite or not.
+
+If it is then print “YES” else print “NO”.
+
+A number is Anshuman’s favourite if it is either the sum or the difference of the integer 5. (5+5, 5+5+5, 5-5,5-5-5+5+5…..)
+
+Input:
+The first line of input contains an integer denoting the number of test cases . Next line of input contains an integer N to be tested.    
+
+Output:
+For each test case , the output is in a new line containg the answer 'YES' or 'NO' depending on whether the given number N is Anshuman's favourite or not .
+
+Constraints:
+1<=T<=100
+-10^9<=N<=10^9
+
+Example:
+Input : 
+1
+10
+Output : 
+YES
+
+Because 10 can be written as a sum of 5+5.
+
+Example:
+Input : 
+1
+9
+Output : 
+NO
+
+Input :
+2
+-255
+389
+Output :
+YES
+NO
+*/
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+ {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+ //int n,i;cin>>n;
+ //int a[n];for(i=0;i<n;i++)cin>>a[i];
+ long long int n;
+ cin>>n;
+ if(n %5 == 0)
+ cout<<"YES";
+ else cout<<"NO";
+ cout<<endl;
+ }
+	//code
+	return 0;
+}
