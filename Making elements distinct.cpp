8cd287d62046f1781b090e2b021da67fd77982f1
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-03-31 09:40:02 +0530

    Update Making elements distinct.cpp

--- Making elements distinct.cpp
+++ Making elements distinct.cpp
@@ -31,7 +31,92 @@ Test Case 1: We make the array as {2, 3, 4, 5, 6}. Sum becomes 2 + 3 + 4 + 5 + 6
 
 Test Case2 : We make the array as  {20, 21 } .sum = 41
 */
+/*
+GFG:
+// Efficient CPP program to make sorted array 
+// elements distinct by incrementing elements 
+// and keeping sum to minimum. 
+#include <iostream> 
+using namespace std; 
+O(n)
+// To find minimum sum of unique elements. 
+int minSum(int arr[], int n) 
+{ 
+	int sum = arr[0], prev = arr[0]; 
 
+	for (int i = 1; i < n; i++) { 
+
+		// If violation happens, make current 
+		// value as 1 plus previous value and 
+		// add to sum. 
+		if (arr[i] <= prev) { 
+			prev = prev + 1; 
+			sum = sum + prev; 
+		} 
+
+		// No violation. 
+		else { 
+			sum = sum + arr[i]; 
+			prev = arr[i]; 
+		} 
+	} 
+
+	return sum; 
+} 
+
+// Drivers code 
+int main() 
+{ 
+	int arr[] = { 2, 2, 3, 5, 6 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	cout << minSum(arr, n) << endl; 
+	return 0; 
+} 
+
+
+
+
+//O(n^2)
+// CPP program to make sorted array elements 
+// distinct by incrementing elements and keeping 
+// sum to minimum. 
+#include <iostream> 
+using namespace std; 
+
+// To find minimum sum of unique elements. 
+int minSum(int arr[], int n) 
+{ 
+	int sum = arr[0]; 
+
+	for (int i = 1; i < n; i++) { 
+		if (arr[i] == arr[i - 1]) {			 
+
+			// While current element is same as 
+			// previous or has become smaller 
+			// than previous. 
+			int j = i; 
+			while (j < n && arr[j] <= arr[j - 1]) {		 
+				arr[j] = arr[j] + 1; 
+				j++; 
+			} 
+		} 
+		sum = sum + arr[i]; 
+	} 
+
+	return sum; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int arr[] = { 2, 2, 3, 5, 6 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+	cout << minSum(arr, n) << endl; 
+	return 0; 
+} 
+
+
+*/
 #include<bits/stdc++.h>
 #include<iostream>
 
