efd6a4abbdac78290b393cb565d638d8533d092f
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-04-28 20:47:53 +0530

    Create Largest number possible.cpp

---
+++ Greedy/Largest number possible.cpp
@@ -0,0 +1,82 @@
+/*
+Largest number possible
+Given two numbers 'N' and 'S' , find the largest number that can be formed with 'N' digits and whose sum of digits should be equals to 'S'.
+ 
+
+Input
+
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. The first line of each test case contains two space separated integers N and S, where N is the number of digits and S is the sum.
+ 
+
+Output
+
+Print the largest number that is possible.
+If their is no such number, then print -1 
+ 
+
+Constraints:
+
+1 <= T <= 30
+1 <= N <= 50
+0 <= S <= 500
+ 
+
+Example 
+
+Input
+2
+2 9
+3 20
+
+Output 
+
+90
+992 
+
+Expected Time Complexity: O(n)
+ 
+*/
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void ns(int m,int s)
+{
+   int r[m];
+   for(int i=0;i<m;i++)
+   {
+       if(s>=9)
+       {
+           r[i]=9;
+           s-=9;
+       }
+       else
+       {
+           r[i]=s;
+           s=0;
+       }
+   }
+   for(int i=0;i<m;i++)
+   cout<<r[i];
+}
+int main()
+ {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+ //int n,i;cin>>n;
+ //int a[n];for(i=0;i<n;i++)cin>>a[i];
+ int n,s;
+ cin>>n>>s;
+ if((n!=1 && s==0)|| s>9*n)
+ {
+     cout<<"-1";
+ }
+ else {
+     ns(n,s);
+ }
+ cout<<endl;
+ }
+	//code
+	return 0;
+}
